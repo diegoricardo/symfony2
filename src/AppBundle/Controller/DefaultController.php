d3456b28604f6f48e17beef2dbadf80c4a91d6c5
Diego Ricardo <diego.r.araujo@gmail.com>
2014-12-25 19:31:25 -0200

    Symfony2 e primeira action run

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/run/{marca}/{modelo}", name="run")
+     */
+    public function runAction($marca, $modelo)
+    {
+        return $this->render('AppBundle:Default:run.html.twig', [
+        		'marca' => $marca,
+        		'modelo' => $modelo
+        	]
+    	);
+    }
+}
