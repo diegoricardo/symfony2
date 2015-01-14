9d6f95de9d4028a0c16fd9c52ae906e2020d1064
Diego Ricardo <diego.r.araujo@gmail.com>
2015-01-14 21:44:44 -0200

    Fase 2 Projeto: Criando Bundle

---
+++ src/Code/CarBundle/Controller/CarController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Code\CarBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CarController extends Controller
+{
+    /**
+     * @Route("/index")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $marcas = ['GM/Chevrolet', 'Honda', 'Jac Motors', 'Lifan', 'Nissan', 'Peugeot', 'Volkswagen'];
+        $modelos = ['Cobalt', 'Astra Sedan', 'City', 'J3 Turin', '530', 'Tiida Sedan', '207 Sedan', 'Polo Sedan'];
+
+        return [
+            'marcas' => $marcas,
+            'modelos' => $modelos,
+        ];
+    }
+}
